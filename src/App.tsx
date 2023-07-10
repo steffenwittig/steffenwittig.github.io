f17a83cc86d4a96e3ab876d60426b8fa9aac01f4
Steffen Wittig <steffenwittig@steffenwittig.com>
2023-07-10 20:19:57 +0200

    Add breadcrumbs (refactor routing)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
-import { HashRouter, Link, Route, Routes } from 'react-router-dom'
+import { Outlet, RouterProvider, createHashRouter } from 'react-router-dom'
+import { Breadcrumbs } from './Components/Breadcrumbs/Breadcrumbs'
+import { CookieNotice } from './Components/CookieNotice/CookieNotice'
 import { Footer } from './Components/Footer/Footer'
+import { ArtPortfolioPage } from './Pages/ArtPortfolioPage'
+import { CookieDimension } from './Pages/CookieDimension'
+import { DataPrivacyPage } from './Pages/DataPrivacyPage'
 import { HomePage } from './Pages/HomePage'
 import { ImpressPage } from './Pages/ImpressPage'
-import { DataPrivacyPage } from './Pages/DataPrivacyPage'
-import { ArtPortfolioPage } from './Pages/ArtPortfolioPage'
-import { CookieNotice } from './Components/CookieNotice/CookieNotice'
-import { CookieDimension } from './Pages/CookieDimension'
 
-const App = () => {
-  return (
-    <div className="container">
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/art-portfolio" element={<ArtPortfolioPage />} />
-          <Route path="/impress" element={<ImpressPage />} />
-          <Route path="/data-privacy" element={<DataPrivacyPage />} />
-          <Route path="/cookie-dimension" element={<CookieDimension />} />
-        </Routes>
+const router = createHashRouter([
+  {
+    path: '/',
+    element: (
+      <div className="container">
+        <Breadcrumbs />
+        <Outlet />
         <CookieNotice />
         <Footer />
-      </HashRouter>
-    </div>
-  )
+      </div>
+    ),
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/art-portfolio',
+        element: <ArtPortfolioPage />,
+        handle: { title: 'NotSteffenWittig Art' },
+      },
+      {
+        path: '/impress',
+        element: <ImpressPage />,
+        handle: { title: 'Impress' },
+      },
+      {
+        path: '/data-privacy',
+        element: <DataPrivacyPage />,
+        handle: { title: 'Impress' },
+      },
+      {
+        path: '/cookie-dimension',
+        element: <CookieDimension />,
+        handle: { title: 'Cookie Dimension' },
+      },
+    ],
+    handle: { title: 'Home' },
+    errorElement: <p>Page not found</p>,
+  },
+])
+
+const App = () => {
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/Components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,40 @@
+import { Link, useMatches } from 'react-router-dom'
+import styles from './Breadcrumbs.module.scss'
+
+interface ICrumb {
+  pathname: string
+  title: string
+}
+
+export const Breadcrumbs = () => {
+  const matches = useMatches()
+  const crumbs: ICrumb[] = matches
+    // @ts-ignore the react-router useMatches solution for breadcrumbs is special...
+    .filter((match) => Boolean(match.handle?.title))
+    .map(
+      (match): ICrumb => ({
+        pathname: match.pathname,
+        // @ts-ignore
+        title: match.handle?.title,
+      })
+    )
+
+  function renderCrumb(crumb: ICrumb, level: number, withLink: boolean) {
+    if (withLink) {
+      return (
+        <Link to={crumb.pathname} title={crumb.title}>
+          {renderSegment(crumb, level)}
+        </Link>
+      )
+    }
+    return renderSegment(crumb, level)
+  }
+
+  function renderSegment(crumb: ICrumb, level: number) {
+    return <li key={level}>{crumb.title}</li>
+  }
+
+  return (
+    <ol className={styles.breadcrumbs}>{crumbs.map((c: ICrumb, i) => renderCrumb(c, i, i < crumbs.length - 1))}</ol>
+  )
+}
