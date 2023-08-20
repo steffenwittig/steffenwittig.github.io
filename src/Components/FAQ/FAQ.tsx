8f3ca9a9db1a2435ea7502c8abb8b8a0f69c28cf
Steffen Wittig <steffenwittig@steffenwittig.com>
2023-08-20 09:44:48 +0200

    FAQ Component

---
+++ src/Components/FAQ/FAQ.tsx
@@ -0,0 +1,17 @@
+import { ElementType } from 'react'
+import { FAQItem, IFAQItemProps } from './FAQItem'
+
+interface IFAQProps {
+  items: IFAQItemProps[]
+  headingTag?: ElementType
+}
+
+export const FAQ = ({ items, headingTag }: IFAQProps) => {
+  return (
+    <>
+      {items.map((item) => (
+        <FAQItem {...item} headingTag={headingTag} key={item.question} />
+      ))}
+    </>
+  )
+}
