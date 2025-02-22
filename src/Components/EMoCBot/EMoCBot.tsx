3dc6bf6c507086c5da432b61fa7b82b1f62876ec
Steffen Wittig <github@steffenwittig.com>
2025-02-22 09:09:45 +0100

    Fix EMoCBot wrong dialogue after page load, add spacer to bottom so footer isn't hidden on small screens

--- src/Components/EMoCBot/EMoCBot.tsx
+++ src/Components/EMoCBot/EMoCBot.tsx
@@ -1,7 +1,7 @@
 import { MinusCircle } from '@styled-icons/boxicons-regular'
 import { useLocalStorage } from '@uidotdev/usehooks'
 import { Button } from 'Components/Button/Button'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styles from './EMoCBot.module.scss'
 import { Option, tree } from './EMoCBotDialogue'
 import { EmoCBotMessage, EmoCBotMessageProps, EmoCBotMessageSender } from './EMoCBotMessage'
@@ -18,11 +18,11 @@ export const EMoCBot = () => {
   const [messages, setMessages] = useLocalStorage<EmoCBotMessageProps[]>('bot.messages', [
     { id: DEFAULT_TREE_ID, sender: EmoCBotMessageSender.Bot, text: tree[DEFAULT_TREE_ID].text },
   ])
-  const [dialogueId, setDialogueId] = useLocalStorage<string>('bot.dialogueId', 'intro')
-
-  const [isExpanded, setIsExpanded] = useState<boolean>(true)
-  const [options, setOptions] = useState<Option[]>(getValidOptions(tree[DEFAULT_TREE_ID].options))
-  const messageArea = useRef<HTMLDivElement>(null)
+  const [dialogueId, setDialogueId] = useLocalStorage<string>('bot.dialogueId', DEFAULT_TREE_ID)
+  const [isExpanded, setIsExpanded] = useLocalStorage<boolean>('bot.isExpanded', false)
+  const [options, setOptions] = useState<Option[]>(getValidOptions(tree[dialogueId].options))
+  const headerArea = useRef<HTMLDivElement>(null)
+  const [bottomSpace, setBottomSpace] = useState<number>(0)
 
   const onOptionSelect = (option: Option) => {
     setOptions([])
@@ -39,7 +39,7 @@ export const EMoCBot = () => {
       option.action()
     }
 
-    const delay = 1000
+    const delay = 1000 + Math.random() * 2000
 
     const newMessageBot: EmoCBotMessageProps = {
       id: 'bot' + Date.now().toString(),
@@ -69,6 +69,12 @@ export const EMoCBot = () => {
     }, delay)
   }
 
+  useEffect(() => {
+    if (headerArea.current && headerArea.current.clientHeight) {
+      setBottomSpace(headerArea.current?.clientHeight)
+    }
+  }, [headerArea])
+
   const faceProps: EmoCFaceProps = {
     arousal: 0,
     valence: 0,
@@ -77,25 +83,27 @@ export const EMoCBot = () => {
   }
 
   return (
-    <div className={styles.outer} data-is-expanded={isExpanded}>
-      <div className={styles.headerArea}>
-        <EMoCFace {...faceProps} />
-        <div className={styles.title}>EmoCBot</div>
-        <div>{dialogueId}</div>
-        <Button variant="link" title={'Close'} onClick={() => setIsExpanded(!isExpanded)} aria-expanded={isExpanded}>
-          <MinusCircle className={styles.toggle} />
-        </Button>
+    <>
+      <div className={styles.outer} data-is-expanded={isExpanded}>
+        <div className={styles.headerArea} ref={headerArea}>
+          <EMoCFace {...faceProps} />
+          <div className={styles.title}>EmoCBot</div>
+          <Button variant="link" title={'Close'} onClick={() => setIsExpanded(!isExpanded)} aria-expanded={isExpanded}>
+            <MinusCircle className={styles.toggle} />
+          </Button>
+        </div>
+        <div className={styles.messageArea}>
+          {messages.map((m) => (
+            <EmoCBotMessage {...m} key={m.id} />
+          ))}
+        </div>
+        <div className={styles.inputArea}>
+          {options.map((o) => (
+            <Button title={o.text} onClick={() => onOptionSelect(o)} variant="border" key={o.text + o.reactionId} />
+          ))}
+        </div>
       </div>
-      <div className={styles.messageArea} ref={messageArea}>
-        {messages.map((m) => (
-          <EmoCBotMessage {...m} key={m.id} />
-        ))}
-      </div>
-      <div className={styles.inputArea}>
-        {options.map((o) => (
-          <Button title={o.text} onClick={() => onOptionSelect(o)} variant="border" key={o.text + o.reactionId} />
-        ))}
-      </div>
-    </div>
+      <div className={styles.bottomSpace} style={{ height: bottomSpace }}></div>
+    </>
   )
 }
